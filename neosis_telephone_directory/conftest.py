743cde4c90bd3b0783467f5923bcb37d66e27743
faiz borkar <faiz.borkar@neosoftmail.com>
2020-12-07 10:53:43 +0530

    Project Initialization with User Authenticatino

---
+++ neosis_telephone_directory/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from neosis_telephone_directory.users.models import User
+from neosis_telephone_directory.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
