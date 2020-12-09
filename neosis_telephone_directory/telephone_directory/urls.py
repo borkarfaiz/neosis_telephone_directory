d33c4c3207ec969ca06e8d879024d9ad43e84f66
faiz borkar <faiz.borkar@neosoftmail.com>
2020-12-09 16:48:17 +0530

    basic detailview with html and css linked

--- neosis_telephone_directory/telephone_directory/urls.py
+++ neosis_telephone_directory/telephone_directory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import contact_list_view2, contact_create_view
+from .views import contact_list_view2, contact_create_view, contact_detail_view
 
 app_name = "contacts"
 urlpatterns = [
     path("", view=contact_list_view2, name="contact_list_view"),
     path("create", view=contact_create_view, name="contact_create_view"),
+    path("<slug:pk>", view=contact_detail_view, name="contact_detail_view"),
 ]
--- neosis_telephone_directory/telephone_directory/views.py
+++ neosis_telephone_directory/telephone_directory/views.py
@@ -1,44 +1,21 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 from django.views.generic.edit import ModelFormMixin
 from django.shortcuts import render, reverse
 from django.core.exceptions import  ValidationError
 
-from django_tables2 import RequestConfig
-from django_tables2 import LazyPaginator
+from django_filters.views import FilterView
+from django_tables2 import LazyPaginator, SingleTableMixin, RequestConfig 
 
 from .filters import ContactsFilter
 from .forms import PictureForm
 from .models import Contacts
 from .tables import ContactsTable
 
+from django_tables2 import SingleTableMixin
 
 User = get_user_model()
-
-class ContactListView(LoginRequiredMixin, ListView):
-    model = Contacts
-    slug_field = "id"
-    slug_url_kwarg = "id"
-
-contact_list_view1 = ContactListView.as_view()
-
-
-# views.py
-def contact_list_view2(request):
-    contacts = Contacts.objects.filter(user=request.user)
-    # contacts = ContactsFilter(request.GET, queryset=contacts)
-    table = ContactsTable(contacts)
-    RequestConfig(
-        request, paginate={'per_page': 20, "paginator_class": LazyPaginator}
-    ).configure(table)
-    return render(request, "telephone_directory/contacts_list.html", {
-        "table": table
-    })
-
-
-from django_tables2 import SingleTableMixin
-from django_filters.views import FilterView
 
 
 class FilteredPersonListView(SingleTableMixin, FilterView):
@@ -72,24 +49,14 @@ class ContactCreateView(LoginRequiredMixin, CreateView):
         print('form_valid')
         self.object = form.save(commit=False)
         self.object.user = self.request.user
-        # if self.object.profile_pic:
-        #     file_size = self.object.profile_pic._file.size
-        #     five_hundred_kb = 1024 * 100
-        #     if file_size > five_hundred_kb:
-        #         raise ValidationError("Image file too large ( > 500kb )")
-
         self.object.save()
         return super(ModelFormMixin, self).form_valid(form)
-
-    # def clean_profile_pic(self, form):
-    #     print('clean_profile_pic')
-    #     image = self.object.get('profile_pic')
-    #     if image:
-    #         # if image._size > 4*1024*1024:
-    #         if image._size > 4:
-    #             raise ValidationError("Image file too large ( > 4mb )")
-    #         return image
 
 contact_create_view = ContactCreateView.as_view()
 
 
+class ContactsDetailView(LoginRequiredMixin, DetailView):
+    model = Contacts
+
+
+contact_detail_view = ContactsDetailView.as_view()
