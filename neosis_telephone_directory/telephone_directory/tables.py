6ff27f02ea0f11f46991f4bcd87fe85c2007bd2e
faiz borkar <faiz.borkar@neosoftmail.com>
2020-12-11 15:14:06 +0530

    added local production yml file

--- neosis_telephone_directory/telephone_directory/tables.py
+++ neosis_telephone_directory/telephone_directory/tables.py
@@ -1,11 +1,18 @@
 import itertools
 
 import django_tables2 as tables
+from django_tables2.utils import A
 
 from .filters import ContactsFilter
 from .models import Contacts
 
 class ContactsTable(tables.Table):
+    details = tables.LinkColumn(
+        'contacts:contact_detail_view', 
+        text="click here", 
+        attrs={"a": {"style": "color: green;"},}, 
+        args=[A('pk')]
+    )
     No = tables.Column(empty_values=(), orderable=False)
     mobile_number = tables.Column(orderable=False)
     landline_number = tables.Column(orderable=False)
@@ -20,7 +27,7 @@ class ContactsTable(tables.Table):
         model = Contacts
         attrs = {"class": "table table-striped"}
         sequence = [
-            'No', "first_name", "middle_name", 'last_name', 'mobile_number', 
-            'landline_number', 'created',
+            'No', "first_name", "middle_name", 'last_name', 'mobile_number',
+            'landline_number', 'created', 'notes', 'details'
         ]
         exclude = ["user", "email", "modified", "id", "profile_pic"]
